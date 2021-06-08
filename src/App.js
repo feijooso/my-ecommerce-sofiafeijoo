428bcb0342b7ece2e9bd88da67c6102e86ec0c2c
feijooso <sfeijoo@fi.uba.ar>
2021-06-08 00:48:19 -0300

    primer entrega: crear la app utilizando el cli

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Las plantas de Sofi</h1>
+      <h2>Coming soon</h2>
+      <p>Soy Sofi y voy a estar vendiendo plantas en este sitio proximamente :)</p>
     </div>
+    
   );
 }
 
