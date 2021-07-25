216e9e7fac52a40ef041c89e44ba99df887783f0
feijooso <sfeijoo@fi.uba.ar>
2021-07-25 14:21:20 -0300

    checkout now working

--- src/views/checkout/checkout.js
+++ src/views/checkout/checkout.js
@@ -15,18 +15,17 @@ function Checkout() {
         //setOrderId(data.id);
     };
  */
-    const addOrder = useEffect((values) => {
-        (async(values)=> {
-            const fecha = new Date();
-		    const res = await itemsCollection.collection('ordenes').add({...cart, nombre: values.nombre, telefono: values.telefono, mail: values.mail, fecha: fecha}).then(data=> data.id);
-            setOrderId(res.id);
-        })();
-    }, [orderId])
+    const addOrder = async(values)=> {
+        const fecha = new Date();
+        await itemsCollection.collection('ordenes')
+        .add({...cart, nombre: values.nombre, telefono: values.telefono, mail: values.mail, fecha: fecha})
+        .then(data=> setOrderId(data.id));
+    };
 
     return (
         <div>
             <Form addOrder={addOrder} />
-            {orderId != "" && <p>Orden registrada con ID {orderId}</p>}
+            {orderId != "" && <p>Orden registrada con ID: {orderId}</p>}
         </div>
     )
 }
