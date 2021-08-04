39ed7b35d247ceb07095799dbcd13a86302abee4
feijooso <sfeijoo@fi.uba.ar>
2021-08-04 03:00:56 -0300

    contact view

--- src/views/contacto/contacto.js
+++ src/views/contacto/contacto.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import MailOutlinedIcon from '@material-ui/icons/MailOutlined';
+import PhoneOutlinedIcon from '@material-ui/icons/PhoneOutlined';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 
-function contacto() {
-    return (
-        <div>
-            Contacto
-        </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+function Contacto() {
+    const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>img</Paper>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+              <h3>Contactanos</h3>
+              <MailOutlinedIcon/><p>plantasdesofi@gmail.com</p>
+              <PhoneOutlinedIcon/><p>11 2232-2342</p>
+              <HomeOutlinedIcon/><p>Av Avenida 2334 3D, CABA</p>
+
+          </Paper>
+        </Grid>
+        
+      </Grid>
+    </div>
+  );
 }
 
-export default contacto
+export default Contacto
